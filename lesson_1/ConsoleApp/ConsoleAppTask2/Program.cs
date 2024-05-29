efd41ed1df0e03bcb83e42ac16cfc03d2c05445e
Dzmitry-Patapau <potapovdimitry@gmail.com>
2024-05-29 12:00:51 +0300

    Lesson_1

---
+++ lesson_1/ConsoleApp/ConsoleAppTask2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}

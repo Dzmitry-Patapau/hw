78d00171bea6edaceefbd8306b795c934315605b
Dzmitry-Patapau <potapovdimitry@gmail.com>
2024-05-29 12:35:57 +0300

    Lesson_1

--- lesson_1/ConsoleApp/ConsoleAppTask2/Program.cs
+++ lesson_1/ConsoleApp/ConsoleAppTask2/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
     }
